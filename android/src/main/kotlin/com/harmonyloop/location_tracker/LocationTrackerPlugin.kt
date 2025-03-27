a4ba717258fcdc04e663db829de9d687a0063ada
Shuhaib kt <shuhaibkt02@gmail.com>
2025-03-27 13:09:52 +0530

    first issue triggerd

--- android/src/main/kotlin/com/harmonyloop/location_tracker/LocationTrackerPlugin.kt
+++ android/src/main/kotlin/com/harmonyloop/location_tracker/LocationTrackerPlugin.kt
@@ -11,7 +11,8 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
 
-class LocationTrackerPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, PluginRegistry.RequestPermissionsResultListener {
+class LocationTrackerPlugin : FlutterPlugin, ActivityAware, MethodChannel.MethodCallHandler, PluginRegistry.RequestPermissionsResultListener {
+
     private lateinit var channel: MethodChannel
     private var activity: Activity? = null
     private var locationService: LocationService? = null
@@ -147,14 +148,28 @@ class LocationTrackerPlugin : FlutterPlugin, MethodChannel.MethodCallHandler, Pl
         }
     }
 
-    override fun onDetachedFromActivity() {
-        if (isBound) {
-            activity?.unbindService(connection)
-            isBound = false
-            Log.d(TAG, "Unbound from service")
-        }
-        activity = null
+override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    this.activity = binding.activity
+    bindToService()
+}
+
+override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+}
+
+override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    this.activity = binding.activity
+}
+
+override fun onDetachedFromActivity() {
+    if (isBound) {
+        activity?.unbindService(connection)
+        isBound = false
+        Log.d(TAG, "Unbound from service")
     }
+    activity = null
+}
+
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray): Boolean {
         if (requestCode == REQUEST_LOCATION_PERMISSION) {
